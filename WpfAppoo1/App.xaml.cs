d1d5c4af50bbec849788f21f515db74dccc2b453
lanabaka123 <denis.yakimoff@mail.ru>
2026-03-23 13:15:03 +0700

    bebro

--- WpfAppoo1/App.xaml.cs
+++ WpfAppoo1/App.xaml.cs
@@ -13,5 +13,11 @@ namespace WpfAppoo1
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            AuthorizationWindow authWindow = new AuthorizationWindow();
+            authWindow.Show();
+        }
     }
 }
